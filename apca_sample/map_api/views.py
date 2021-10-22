6b7ff70c61e83283987fac3baad6cd1854e5c4cc
Hillary <khan.hillary@gmail.com>
2021-10-22 16:54:06 -0400

    creates serializer for api

--- apca_sample/map_api/views.py
+++ apca_sample/map_api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import StatSerializer
 
 
 # Create your views here.
 @api_view(['GET'])
 def county_stats(request):
-    # serializer = 
-    # return Response(serializer.data)
-    return
+    serializer = StatSerializer(data=request.data)
+    return Response(serializer.data)
